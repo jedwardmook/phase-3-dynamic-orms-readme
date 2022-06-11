cfea4208e8599cc3f00b12707f739a7b80c12d28
JedwardMook <jedwardmook@yahoo.com>
2022-06-11 10:59:36 -0500

    commit tree 9231f19e510250c937c6bb5df4abd345f99fc37b

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require "pry"
 
 class Song
 
@@ -9,6 +10,7 @@ class Song
   end
 
   def self.column_names
+    binding.pry
     DB[:conn].results_as_hash = true
 
     sql = "pragma table_info('#{table_name}')"
